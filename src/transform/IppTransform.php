065bf7c7ad31880d2c5d5de9207a3279cda0ab1d
Zein Miftah <zeinmiftah@gmail.com>
2019-04-02 11:20:21 +0700

    Update transform class

--- src/transform/IppTransform.php
+++ src/transform/IppTransform.php
@@ -9,74 +9,66 @@ use yii\db\ActiveRecordInterface;
  */
 class IppTransform implements DataTransformInterface
 {
-	use AccessorTrait;
+    use AccessorTrait;
 
-	/**
-	 * @var Ipp
-	 */
-	private $model;
-	/**
-	 * @var object
-	 */
-	private $params;
+    /**
+     * @var Ipp
+     */
+    public $model;
 
-	/**
-	 * @inheritdoc
-	 */
-	public function __construct(ActiveRecordInterface $model)
-	{
-		$this->model = $model;
-		$this->program = $model->program;
-		$this->creator = $model->creator;
-		$this->marketer = $model->marketer;
-	}
-	/**
-	 * @inheritdoc
-	 */
-	public function getParams()
-	{
-		if (!is_null($this->params)) {
-			return $this->params;
-		}
+    /**
+     * @inheritdoc
+     */
+    public function __construct(ActiveRecordInterface $model)
+    {
+        $this->model = $model;
+        $this->program = $model->program;
+        $this->creator = $model->creator;
+        $this->marketer = $model->marketer;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getParams()
+    {
+        $object = new \stdClass;
+        $object->id          = $this->model->id;
+        $object->title       = $this->model->program_name;
+        $object->no          = $this->model->ipp_no;
+        $object->ipp_date    = date_format(date_create($this->model->ipp_date), "d-m-Y");
+        $object->exec_date   = $this->model->getExecutionDate("d-m-Y");
+        $object->program     = $this->model->program_id ? $this->model->program_name : "-";
+        $object->amount      = $this->getAmount();
+        $object->donatur     = $this->model->donor_id ? $this->model->donatur->text : "-";
+        $object->marketer    = $this->model->marketer_id ? $this->model->marketer->name : "-";
+        $object->creator     = $this->model->created_by ? $this->model->creator->name : "-";
+        $object->approver    = $this->model->approved_by ? $this->model->approver->name : "-";
+        $object->location    = $this->model->getLocation();
+        $object->company     = $this->model->company_id;
+        $object->year        = date('Y', strtotime($this->model->ipp_date));
+        $object->is_ramadhan = $this->model->isRamadhan;
 
-		$object = new \stdClass;
-		$object->id          = $this->model->id;
-		$object->title       = $this->model->program_name;
-		$object->no          = $this->model->ipp_no;
-		$object->ipp_date    = date_format(date_create($this->model->ipp_date), "d-m-Y");
-		$object->exec_date   = $this->model->getExecutionDate("d-m-Y");
-		$object->program     = $this->model->program_id ? $this->model->program_name : "-";
-		$object->amount      = $this->getAmount();
-		$object->donatur     = $this->model->donor_id ? $this->model->donatur->text : "-";
-		$object->marketer    = $this->model->marketer_id ? $this->model->marketer->name : "-";
-		$object->creator     = $this->model->created_by ? $this->model->creator->name : "-";
-		$object->approver    = $this->model->approved_by ? $this->model->approver->name : "-";
-		$object->location    = $this->model->getLocation();
-		$object->company     = $this->model->company_id;
-		$object->year        = date('Y', strtotime($this->model->ipp_date));
-		$object->is_ramadhan = $this->model->isRamadhan;
-
-		return $this->params = $object;
-	}
-	/**
-	 * @inheritdoc
-	 */
-	public function getModel()
-	{
-		return $this->model;
-	}
-	/**
-	 * Get formatted total amount of Ipp
-	 *
-	 * @return string
-	 */
-	protected function getAmount()
-	{
-		if ($total_amount = $this->model->total_amount) {
-			$symbol = $this->model->getCurrencyCode('symbol');
-			$amount = number_format($total_amount, 0, ',', '.');
-			return sprintf('%s. %s', $symbol, $amount);
-		}
-		return "-";
-	}
+        return $this->params = $object;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+    /**
+     * Get formatted total amount of Ipp
+     *
+     * @return string
+     */
+    protected function getAmount()
+    {
+        if ($total_amount = $this->model->total_amount) {
+            $symbol = $this->model->getCurrencyCode('symbol');
+            $amount = number_format($total_amount, 0, ',', '.');
+            return sprintf('%s. %s', $symbol, $amount);
+        }
+        return "-";
+    }
 }
--- src/transform/ProjectTransform.php
+++ src/transform/ProjectTransform.php
@@ -9,49 +9,49 @@ use yii\db\ActiveRecordInterface;
  */
 class ProjectTransform implements DataTransformInterface
 {
-	use AccessorTrait;
-	
-	/**
-	 * @var Project
-	 */
-	public $model;
+    use AccessorTrait;
+    
+    /**
+     * @var Project
+     */
+    public $model;
 
-	/**
-	 * @inheritdoc
-	 */
-	public function __construct(ActiveRecordInterface $model)
-	{
-		$this->model = $model;
-		$this->program = $model->program;
-		$this->creator = $model->creator;
-	}
-	/**
-	 * @inheritdoc
-	 */
-	public function getParams()
-	{
-		$object = new \stdClass;
-		$object->id           = $this->model->id;
-		$object->no           = $this->model->project_no;
-		$object->name         = $this->model->project_name;
-		$object->project_date = date("d-m-Y", strtotime($this->model->project_date));
-		$object->exec_date    = date("d-m-Y", strtotime($this->model->realize_date_start));
-		$object->program      = $this->model->program ? $this->model->project_name : "-";
-		$object->pic          = $this->model->pic ? $this->model->pic->full_name : "-";
-		$object->creator      = $this->model->creator ? $this->model->creator->full_name : "-";
-		$object->verifikator  = $this->model->verificator ? $this->model->verificator->full_name : "-";
-		$object->location     = $this->model->getLocation();
-		$object->last_file    = $this->model->lastUploadedFile ? $this->model->lastUploadedFile->file : null;
-		$object->year         = date('Y', strtotime($this->model->project_date));
-		$object->is_ramadhan  = $this->model->isRamadhan;
-		// mitra = $model->cpm ? $model->cpm->partner->partner_name : "-",
-		return $object;
-	}
-	/**
-	 * @inheritdoc
-	 */
-	public function getModel()
-	{
-		return $this->model;
-	}
+    /**
+     * @inheritdoc
+     */
+    public function __construct(ActiveRecordInterface $model)
+    {
+        $this->model = $model;
+        $this->program = $model->program;
+        $this->creator = $model->creator;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getParams()
+    {
+        $object = new \stdClass;
+        $object->id           = $this->model->id;
+        $object->no           = $this->model->project_no;
+        $object->name         = $this->model->project_name;
+        $object->project_date = date("d-m-Y", strtotime($this->model->project_date));
+        $object->exec_date    = date("d-m-Y", strtotime($this->model->realize_date_start));
+        $object->program      = $this->model->program ? $this->model->project_name : "-";
+        $object->pic          = $this->model->pic ? $this->model->pic->full_name : "-";
+        $object->creator      = $this->model->creator ? $this->model->creator->full_name : "-";
+        $object->verifikator  = $this->model->verificator ? $this->model->verificator->full_name : "-";
+        $object->location     = $this->model->getLocation();
+        $object->last_file    = $this->model->lastUploadedFile ? $this->model->lastUploadedFile->file : null;
+        $object->year         = date('Y', strtotime($this->model->project_date));
+        $object->is_ramadhan  = $this->model->isRamadhan;
+        // mitra = $model->cpm ? $model->cpm->partner->partner_name : "-",
+        return $object;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
 }
